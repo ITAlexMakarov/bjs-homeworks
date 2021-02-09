397ba649031514a4c3223dcffb8b225f3d36fdef
Aleks Makarov <it.aleksmakarov@gmail.com>
2021-02-09 21:05:07 +0300

    Выполнил работу над ошибками

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,34 +1,29 @@
+"use strict";
 function getResult(a,b,c)   {
-    "use strict";
     let D = (b ** 2) - (4 * a * c);
     let root = [];
-    if (D < 0)  {
-        return root;    //корней нет
-    }   else if (D == 0) {
+    if (D == 0) {
         let x_1 = ((-b) + (Math.sqrt(D))) / 2 * a;
-        root.push(x_1);
-        return root;    //один корень
+        root.push(x_1);    //один корень
     }   else if (D > 0) {
         let x_1 = ((-b) + (Math.sqrt(D))) / 2 * a;
         let x_2 = ((-b) - (Math.sqrt(D))) / 2 * a;
         root.push(x_1, x_2);    //два корня
-        return root;
     }
+    return root;
 }
 
 function getAverageMark(marks){
     if (marks.length == 0)  {
-        console.log(0);
+        return 0;
     }   else if (marks.length > 5)  {
-        marks.splice(5, 99999);
+        marks.splice(5);
     }
-    let count = marks.length;
     let sum = 0;
     for (let i = 0; i < marks.length; i++) {
         sum += marks[i];
     }
-    let averageMark = sum / count;
-    return averageMark;
+    return sum / marks.length;
 }
 
 function askDrink(name,dateOfBirthday){
