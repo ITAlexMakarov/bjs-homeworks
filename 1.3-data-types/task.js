eee32dd56a6796f8d9967c7e2fc24ed9aa136ab9
Aleks Makarov <it.aleksmakarov@gmail.com>
2021-02-12 16:58:09 +0300

    2 tasks completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,20 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
-}
+  
+  let S = amount - contribution; //тело кредита
+
+  let year = (date.getFullYear() - new Date().getFullYear()); //кол-во лет для кредита
+  let month = date.getMonth() - new Date().getMonth();  //кол-во месяцев для кредита в отдельном году
+  let n = year * 12 + month; //кол-во месяцев
+
+  let hundredthPercent = percent / 100; //сотая часть процента
+  let P = hundredthPercent / 12;  //с.ч.п. приведенная к месяцу
+  let payment = S * (P + P / (((1 + P) ** n) - 1)); //ежем. платёж
+  let totalAmount = new Number(payment * n); //общая сумма 
+  return Number(totalAmount.toFixed(2));
+};
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+  let greeting = name ? `Привет, мир! Меня зовут ${name}.` : `Привет, мир! Меня зовут Аноним.`;
+  return greeting;
 }
